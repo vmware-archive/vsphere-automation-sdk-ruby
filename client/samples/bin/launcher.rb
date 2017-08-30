061d2e254b8b2f0aa6d5e10c5b40b45f09ad47db
pablothx-vmware <pablo.petracini@vmware.com>
2017-08-30 12:34:54 -0300

    Updates

--- client/samples/bin/launcher.rb
+++ client/samples/bin/launcher.rb
@@ -9,7 +9,7 @@
     'list_vms' => 'lib/sample/vcenter/vm/list_vms.rb',
     'power' => 'lib/sample/vcenter/vm/power.rb',
     'create_default_vm' => 'lib/sample/vcenter/vm/create_default_vm.rb',
-    'configure_cpu' => 'lib/sample/vcenter/vm/cpu_configuration.rb',
+    'configure_cpu' => 'lib/sample/vcenter/vm/hardware/cpu_configuration.rb',
 
     # fyi: these are framework self-tests more than samples, but they can be enabled
     # with the understanding that each has its own mechanism for handling arguments.
